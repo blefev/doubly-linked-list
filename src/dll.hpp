a0b11574bf4bd5d13b49d3e54db3b57190cb0fe3
Brendan Lefevre <blefev>
2022-05-16 06:34:06 -0600

    clang-format source files

--- src/dll.hpp
+++ src/dll.hpp
@@ -1,215 +1,179 @@
 #pragma once
 
-#include <cstddef>  // Include to use NULL, otherwise use nullptr
-#include <iostream>
 #include "node.hpp"
+#include <cstddef> // Include to use NULL, otherwise use nullptr
+#include <iostream>
 
-template<class T>
-class DLL{
+template <class T> class DLL {
 private:
-    Node<T> *head;          // the pointer to the front of the list
-    Node<T> *tail;          // the pointer to the back of the list
-    int count;
+  Node<T> *head; // the pointer to the front of the list
+  Node<T> *tail; // the pointer to the back of the list
+  int count;
+
 public:
-    // desc: Inializes a new, empty list
-	DLL();
+  // desc: Inializes a new, empty list
+  DLL();
 
-    // desc:  Adds new data to the front of the list
-    // param: data The data to add to the list
-	void prepend(T &data);
+  // desc:  Adds new data to the front of the list
+  // param: data The data to add to the list
+  void prepend(T &data);
 
-    // desc:  Adds new data to the end of the list
-    // param: data The data to add to the list
-	void append(T &data);   
+  // desc:  Adds new data to the end of the list
+  // param: data The data to add to the list
+  void append(T &data);
 
-    // desc:    Searches the list for the node that contains data and removes it
-    //          from the list
-    // param:   data The data to be searched for and removed
-    // returns: true if the node was found and removed, false if not
-    bool remove(T data); 
+  // desc:    Searches the list for the node that contains data and removes it
+  //          from the list
+  // param:   data The data to be searched for and removed
+  // returns: true if the node was found and removed, false if not
+  bool remove(T data);
 
-    // desc:    Removes the node at the front of the list
-    // returns: true if the node was successfully removed, false if not
-	bool removeFront(void);
+  // desc:    Removes the node at the front of the list
+  // returns: true if the node was successfully removed, false if not
+  bool removeFront(void);
 
-    // desc:    Removes the node at the back of the list
-    // returns: true if the node was successfully removed, false if not
-	bool removeBack(void);
+  // desc:    Removes the node at the back of the list
+  // returns: true if the node was successfully removed, false if not
+  bool removeBack(void);
 
-    // desc:    Searches the list for a node containing the specified data
-    // param:   data The data to be searched for
-    // returns: A pointer to the node containing the data or NULL if a node
-    //          cannot be found
-	Node<T> *search(T data);
+  // desc:    Searches the list for a node containing the specified data
+  // param:   data The data to be searched for
+  // returns: A pointer to the node containing the data or NULL if a node
+  //          cannot be found
+  Node<T> *search(T data);
 
-    // desc::   Returns the data found at the front of the list
-    // returns: The data at the front of the list
-    T getFront(void);
+  // desc::   Returns the data found at the front of the list
+  // returns: The data at the front of the list
+  T getFront(void);
 
-    // desc::   Returns the data found at the back of the list
-    // returns: The data at the back of the list. Return 0 if empty
-    T getBack(void);
+  // desc::   Returns the data found at the back of the list
+  // returns: The data at the back of the list. Return 0 if empty
+  T getBack(void);
 
-    // desc: Prints the contents of the list from front to back
-	void print();
+  // desc: Prints the contents of the list from front to back
+  void print();
 
-    // deletes a node form a pointer
-    bool deleteFromPtr(Node<T>** dblNodePtr);
+  // deletes a node form a pointer
+  bool deleteFromPtr(Node<T> **dblNodePtr);
 };
 
-
 // constructor
-template<class T>
-DLL<T>::DLL()
-{
-	head = NULL;
-    tail = NULL;
-    count = 0;
+template <class T> DLL<T>::DLL() {
+  head = NULL;
+  tail = NULL;
+  count = 0;
 }
 
-template<class T>
-void DLL<T>::prepend(T &data)
-{
-	Node<T>* node = new Node<T>(data);
+template <class T> void DLL<T>::prepend(T &data) {
+  Node<T> *node = new Node<T>(data);
 
-    if (head == NULL) {
-        head = node;
-        return;
-    }
-
-    node->set_next(head);
+  if (head == NULL) {
     head = node;
-
     return;
+  }
+
+  node->set_next(head);
+  head = node;
+
+  return;
 }
 
-template<class T>
-void DLL<T>::append(T &data)
-{
-    Node<T>** dblNodePtr = &(head);
+template <class T> void DLL<T>::append(T &data) {
+  Node<T> **dblNodePtr = &(head);
 
-    while(*dblNodePtr)
-    {
-        dblNodePtr = &(*dblNodePtr)->next;
-    }
+  while (*dblNodePtr) {
+    dblNodePtr = &(*dblNodePtr)->next;
+  }
 
-    *dblNodePtr = new Node<T>(data);
+  *dblNodePtr = new Node<T>(data);
 }
 
-template<class T>
-bool DLL<T>::removeFront()
-{
-    if (!head) 
-    {
-        return false;
-    }
+template <class T> bool DLL<T>::removeFront() {
+  if (!head) {
+    return false;
+  }
 
-	Node<T>* toShift = head->next;
-	delete head;
-	head = toShift;
+  Node<T> *toShift = head->next;
+  delete head;
+  head = toShift;
 
-	return true;
+  return true;
 }
 
-template<class T>
-bool DLL<T>::removeBack()
-{
-	if(!head) 
-    {
-        return false;
-    }
+template <class T> bool DLL<T>::removeBack() {
+  if (!head) {
+    return false;
+  }
 
-	Node<T>** dblNodePtr = &(head);
+  Node<T> **dblNodePtr = &(head);
 
-	while((*dblNodePtr)->next)
-    {
-		dblNodePtr = &(*dblNodePtr)->next;
-    }
+  while ((*dblNodePtr)->next) {
+    dblNodePtr = &(*dblNodePtr)->next;
+  }
 
-	return deleteFromPtr(dblNodePtr);
+  return deleteFromPtr(dblNodePtr);
 }
 
-template<class T>
-Node<T> *DLL<T>::search(T target)
-{
-    Node<T>* nodePtr = this->head;
+template <class T> Node<T> *DLL<T>::search(T target) {
+  Node<T> *nodePtr = this->head;
 
-	while(nodePtr && nodePtr->get_data() != target)
-    {
-		nodePtr = nodePtr->next;
-    }
+  while (nodePtr && nodePtr->get_data() != target) {
+    nodePtr = nodePtr->next;
+  }
 
-	return nodePtr;
+  return nodePtr;
 }
 
-template<class T>
-void DLL<T>::print()
-{
-    Node<T> *itr = head;
+template <class T> void DLL<T>::print() {
+  Node<T> *itr = head;
 
-    std::cout << "Your List: ";
-	while (itr != NULL)
-	{
-        std::cout << itr->get_data() << " -> ";
-        itr = itr->get_next();
-	}
-    std::cout << "NULL" << std::endl;
+  std::cout << "Your List: ";
+  while (itr != NULL) {
+    std::cout << itr->get_data() << " -> ";
+    itr = itr->get_next();
+  }
+  std::cout << "NULL" << std::endl;
 }
 
-template<class T>
-bool DLL<T>::remove(T target)
-{
-    Node<T>** dblNodePtr = &(this->head);
+template <class T> bool DLL<T>::remove(T target) {
+  Node<T> **dblNodePtr = &(this->head);
 
-    while(*dblNodePtr && (**dblNodePtr).get_data() != target) 
-    {
-        dblNodePtr = &(*dblNodePtr)->next;
-    }
+  while (*dblNodePtr && (**dblNodePtr).get_data() != target) {
+    dblNodePtr = &(*dblNodePtr)->next;
+  }
 
-    return deleteFromPtr(dblNodePtr);
+  return deleteFromPtr(dblNodePtr);
 }
 
-template<class T>
-T DLL<T>::getFront(void)
-{
-    if (!head) 
-    {
-        return 0;
-    }
+template <class T> T DLL<T>::getFront(void) {
+  if (!head) {
+    return 0;
+  }
 
-    return (head->get_data());
+  return (head->get_data());
 }
 
-template<class T>
-T DLL<T>::getBack(void)
-{
-	Node<T>* nodePtr = this->head;
+template <class T> T DLL<T>::getBack(void) {
+  Node<T> *nodePtr = this->head;
 
-	while(nodePtr->get_next())
-    {
-		nodePtr = nodePtr->next;
-    }
+  while (nodePtr->get_next()) {
+    nodePtr = nodePtr->next;
+  }
 
-    if (nodePtr)
-    {
-        return nodePtr->get_data();
-    }
-    else
-    {
-        return 0;
-    }
+  if (nodePtr) {
+    return nodePtr->get_data();
+  } else {
+    return 0;
+  }
 }
 
-template<class T>
-bool DLL<T>::deleteFromPtr(Node<T>** dblNodePtr)
-{
-	if(!*dblNodePtr)
-    {
-		return false;
-    }
+template <class T> bool DLL<T>::deleteFromPtr(Node<T> **dblNodePtr) {
+  if (!*dblNodePtr) {
+    return false;
+  }
 
-	Node<T>* toRemove = *dblNodePtr;
-	*dblNodePtr = toRemove->next;
-	delete toRemove;
-	return true;
+  Node<T> *toRemove = *dblNodePtr;
+  *dblNodePtr = toRemove->next;
+  delete toRemove;
+  return true;
 }
--- src/node.hpp
+++ src/node.hpp
@@ -1,72 +1,49 @@
 #pragma once
 #include <cstddef>
-template<class T>
-class Node{
-    public:
-        T data;
-        Node<T> *next;
-        Node<T> *prev;
-    
-       Node(void);
-       Node(T &data);
-       Node<T>* set_data(T &new_data);
-       Node<T>* set_next(Node<T> *next_node);
-       Node<T>* set_prev(Node<T> *prev_node);
-       T get_data(void);
-       Node<T> *get_next(void);
-       Node<T> *get_prev(void);
+template <class T> class Node {
+public:
+  T data;
+  Node<T> *next;
+  Node<T> *prev;
+
+  Node(void);
+  Node(T &data);
+  Node<T> *set_data(T &new_data);
+  Node<T> *set_next(Node<T> *next_node);
+  Node<T> *set_prev(Node<T> *prev_node);
+  T get_data(void);
+  Node<T> *get_next(void);
+  Node<T> *get_prev(void);
 };
 
-template<class T>
-Node<T>::Node()
-{
-    data = 0;
-    next = NULL;
-    prev = NULL;
+template <class T> Node<T>::Node() {
+  data = 0;
+  next = NULL;
+  prev = NULL;
 }
 
-template<class T>
-Node<T>::Node(T &new_data)
-{
-    data = new_data;
-    next = NULL;
+template <class T> Node<T>::Node(T &new_data) {
+  data = new_data;
+  next = NULL;
 }
 
-template<class T>
- Node<T>* Node<T>::set_data(T &new_data)
-{
-    data = new_data;
-    return this;
+template <class T> Node<T> *Node<T>::set_data(T &new_data) {
+  data = new_data;
+  return this;
 }
 
-template<class T>
-T Node<T>::get_data()
-{
-    return data;
+template <class T> T Node<T>::get_data() { return data; }
+
+template <class T> Node<T> *Node<T>::set_next(Node<T> *next_node_ptr) {
+  next = next_node_ptr;
+  return this;
 }
 
-template<class T>
-Node<T>* Node<T>::set_next(Node<T> *next_node_ptr)
-{
-   next = next_node_ptr;
-   return this;
+template <class T> Node<T> *Node<T>::set_prev(Node<T> *prev_node_ptr) {
+  prev = prev_node_ptr;
+  return this;
 }
 
-template<class T>
-Node<T>* Node<T>::set_prev(Node<T> *prev_node_ptr)
-{
-   prev = prev_node_ptr;
-   return this;
-}
+template <class T> Node<T> *Node<T>::get_next() { return next; }
 
-template<class T>
-Node<T> *Node<T>::get_next()
-{
-    return next;
-}
-
-template<class T>
-Node<T> *Node<T>::get_prev()
-{
-    return prev;
-}
+template <class T> Node<T> *Node<T>::get_prev() { return prev; }
